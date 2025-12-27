35c0a9f6415a8ebce13970f368f187e1c20962de
KovarishxD <kovarishxd@gmail.com>
2025-12-27 21:43:33 +0000

    Making it explicit where I am importing the class.

--- Runnable.py
+++ Runnable.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 from typing import TypeVar, Generic, Sequence, AsyncGenerator
 from collections.abc import Mapping
+import RunnableSequence
 import asyncio
 
 
@@ -42,31 +43,3 @@ class Runnable(Generic[InputT, OutputT, ConfigT]):
         return RunnableSequence([self, newRunnable])
 
 
-class RunnableSequence(Runnable[InputT, OutputT, ConfigT], Generic[InputT, OutputT, ConfigT]):
-    def __init__(self, runnables: Sequence[Runnable]) -> None:
-        super().__init__()
-        self.runnables = runnables
-        self.name = self.__class__.__name__
-    
-    def __str__(self) -> str:
-        return " | ".join(str(runnableName) for runnableName in self.runnables)
-
-    async def _call(self, input: InputT, config: ConfigT | None = None) -> OutputT:
-
-        output: InputT | OutputT = input
-        for runnable in self.runnables:
-            output = await runnable.invoke(output, config)
-
-        return output
-    
-    async def _stream(self, input, config: ConfigT | None = None) -> AsyncGenerator[OutputT, None]:
-
-        output: InputT | OutputT = input
-        for runnable in self.runnables[:-1]:
-            output = await runnable.invoke(output, config)
-        
-        async for fragment in self.runnables[-1].stream(output, config):
-            yield fragment
-
-    def pipe(self, newRunnable: Runnable[OutputT, MidT, ConfigT]) -> RunnableSequence[InputT, MidT, ConfigT]:
-        return RunnableSequence(self.runnables + [newRunnable])
