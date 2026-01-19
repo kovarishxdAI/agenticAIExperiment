d14e7e56654f7a8c7a1c982fc88cad94c2f8ac0a
KovarishxD <kovarishxd@gmail.com>
2026-01-19 21:06:19 +0000

    Fix order of operands

--- runnables/division_runnable.py
+++ runnables/division_runnable.py
@@ -13,24 +13,24 @@ class DivisionRunnable(Runnable[NumberT, NumberT, ConfigT]):
         self.name = self.__class__.__name__
 
     async def _call(self, input: NumberT, config: ConfigT | None = None) -> NumberT:
-        return self.value / input
+        return input / self.value
     
 def testing():
     print('Testing DivisionRunnable:\n')
 
     try:
         print('Test 1: Int input in both constructor and invoke.')
-        test1 = DivisionRunnable(1)
-        results1 = asyncio.run(test1.invoke(4))
+        test1 = DivisionRunnable(4)
+        results1 = asyncio.run(test1.invoke(1))
         assert results1 == 0.25, f"Expected 0.25, got {results1}"
-        print(f"Passed 1 in constructor and 4 in invoke, got {results1}. Test 1 passed.\n")
+        print(f"Passed 4 in constructor and 1 in invoke, got {results1}. Test 1 passed.\n")
 
         print('Test 2: Float input in both constructor and invoke.')
-        test2 = DivisionRunnable(2.3)
-        results2 = asyncio.run(test2.invoke(5.7653423465))
+        test2 = DivisionRunnable(5.7653423465)
+        results2 = asyncio.run(test2.invoke(2.3))
         rounded_result2 = round(results2, 2)
         assert rounded_result2 == 0.4, f"Expected 0.4, got {rounded_result2}"
-        print(f"Passed 2.3 in constructor and 5.7653423465 in invoke, got {rounded_result2}. Test 2 passed.\n")
+        print(f"Passed 5.7653423465 in constructor and 2.3 in invoke, got {rounded_result2}. Test 2 passed.\n")
 
         print("Yeap, all DivisionRunnable tests passed.\n")
 
--- runnables/subtraction_runnable.py
+++ runnables/subtraction_runnable.py
@@ -15,31 +15,31 @@ class SubtractionRunnable(Runnable[NumberT, NumberT, ConfigT]):
 
     async def _call(self, input: NumberT | Iterable, config: ConfigT | None = None) -> NumberT:
         nums = flatten_numbers([input])
-        return self.value - sum(nums)
+        return sum(nums) - self.value
 
 def testing():
     print("Testing SubtractionRunnable:\n")
 
     try:
         print('Test 1: Sequence of numeric inputs.')
-        test1 = SubtractionRunnable(10)
-        results1 = asyncio.run(test1.invoke(4))
+        test1 = SubtractionRunnable(4)
+        results1 = asyncio.run(test1.invoke(10))
         assert results1 == 6, f"Expected 6, got {results1}"
-        print(f"Passed 10 in constructor and 4 in invoke, got {results1}. Test 1 passed.\n")
+        print(f"Passed 4 in constructor and 10 in invoke, got {results1}. Test 1 passed.\n")
 
         print('Test 2: Mix between numeric and iterable inputs.')
-        test2 = SubtractionRunnable(10)
-        results2 = asyncio.run(test2.invoke([5, [1.3, "c", []]]))
+        test2 = SubtractionRunnable(6.3)
+        results2 = asyncio.run(test2.invoke([5, [1.3, "c", []], [3.7]]))
         assert results2 == 3.7, f"Expected 3.7, got {results2}"
-        print(f"Passed 10 in constructor and [5, [1.3, \"c\", []]] in invoke, got {results2}. Test 2 passed.\n")
+        print(f"Passed 6.3 in constructor and [5, [1.3, \"c\", []], [3.7]] in invoke, got {results2}. Test 2 passed.\n")
 
         print('Test 3: Mix between numeric and iterable inputs, and a sequence of runnables')
         test3a = SubtractionRunnable(10)
-        test3b = SubtractionRunnable(100)
+        test3b = SubtractionRunnable(4)
         test3seq = test3a.pipe(test3b)
-        results3 = asyncio.run(test3seq.invoke(["c", [4]]))
-        assert results3 == 94, f"Expected 94, got {results3}"
-        print(f"Passed 10 in constructor, 100 in the second, and [\"c\", [4]] in invoke, got {results3}. Test 3 passed.\n")
+        results3 = asyncio.run(test3seq.invoke(["c", [100]]))
+        assert results3 == 86, f"Expected 86, got {results3}"
+        print(f"Passed 10 in constructor, 4 in the second, and [\"c\", [100]] in invoke, got {results3}. Test 3 passed.\n")
 
         print("Yeap, all SubtractionRunnable tests passed.\n")
 
